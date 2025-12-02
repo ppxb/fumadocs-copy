eff6f97976223fcd3048ba58e0979d15ef6599d0
ppxb <317842449@qq.com>
2025-12-02 09:34:07 +0800

    chore: add section

---
+++ components/landing/section.tsx
@@ -0,0 +1,38 @@
+import SectionSvg from './section-svg'
+
+export default function Section({
+	className,
+	id,
+	crosses,
+	crossesOffset,
+	customPaddings,
+	children
+}: {
+	className: string
+	id: string
+	crosses?: boolean
+	crossesOffset: string
+	customPaddings: boolean
+	children: React.ReactNode
+}) {
+	return (
+		<div
+			id={id}
+			className={`
+      relative
+      ${customPaddings || `py-10 lg:py-16  ${crosses ? '' : ''}`}
+      ${className || ' '}`}
+		>
+			{children}
+
+			<div className="hidden absolute top-0 left-5 w-px h-[calc(100%+30px)] dark:bg-[#26242C] bg-stone-200  pointer-events-none lg:block lg:left-16 xl:left-16" />
+			<div className="hidden absolute top-0 right-5 w-px h-[calc(100%+30px)]  dark:bg-[#26242C] bg-stone-200  pointer-events-none lg:block lg:right-14 xl:right-14" />
+
+			{crosses && (
+				<>
+					<SectionSvg crossesOffset={crossesOffset} />
+				</>
+			)}
+		</div>
+	)
+}
